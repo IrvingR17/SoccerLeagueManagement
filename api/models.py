2fa1c452455c1fe408e22d6d70313dbac55b60b3
Irving <a329679@uach.mx>
2022-09-30 11:31:49 -0600

    First version of models

--- api/models.py
+++ api/models.py
@@ -1,13 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.contrib.auth import User
 from operator import mod
 from unicodedata import name
 from datetime import datetime, date
 import uuid
 
 class Players(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=20, blank=False)
-    last_name = models.CharField(max_length=25, blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     pic = models.ImageField()
 
     GENDERS = (
@@ -16,7 +16,6 @@ class Players(models.Model):
     )
     gender = models.CharField(max_length=1, choices=GENDERS, blank=False)
     phone_number = models.CharField(max_length=10, blank=True)
-    email = models.EmailField(max_length=254, blank=False)
 
     POSITION = (
         ('POR', 'Portero'),
@@ -32,33 +31,19 @@ class Players(models.Model):
     )
     sanction_status = models.CharField(max_length=1, choices=SANCTION_STATUS, default="D")
 
-    def __str__(self):
-        return self.first_name 
-
 class Team_Owner(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=20, blank=False)
-    last_name = models.CharField(max_length=20, blank=False)
-
-    def __str__(self):
-        return self.first_name
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Leagues(models.Model):
     name = models.CharField(max_length=20, blank=False)
-
-    def __str__(self):
-        return self.name
     
 class Teams(models.Model):
     name = models.CharField(max_length=20, blank=False)
-    team_owner_name = models.ForeignKey(Team_Owner, on_delete=models.PROTECT)
+    team_owner = models.ForeignKey(Team_Owner, on_delete=models.PROTECT)
     league_name = models.ForeignKey(Leagues, on_delete=models.PROTECT)
     goals_for = models.IntegerField(default=0, blank=False)
     goals_against = models.IntegerField(default=0, blank=False)
     score = models.IntegerField(default=0, blank=False)
-
-    def __str__(self):
-        return self.name 
 
 class TeamPlayerRecords(models.Model):
     player = models.ForeignKey(Players, on_delete=models.PROTECT)
@@ -67,14 +52,9 @@ class TeamPlayerRecords(models.Model):
     goals = models.IntegerField(default=0, blank=False)
     number = models.IntegerField(default=0, blank=False, unique=True)
 
-    def __str__(self):
-        return "{}_{}".format(self.team.__str__(), self.player.__str__()) 
-
 class Campus(models.Model):
     name = models.CharField(max_length=20, blank=False)
-
-    def __str__(self):
-        return self.name
+    adress = models.CharField(max_length=40, blank=False)
 
 class Field(models.Model):
     name = models.CharField(max_length=20, blank=False)
@@ -84,11 +64,7 @@ class Field(models.Model):
         return self.name
 
 class Referees(models.Model):
-    first_name = models.CharField(max_length=20, blank=False)
-    last_name = models.CharField(max_length=20, blank=False)
-
-    def __str__(self):
-        return self.first_name
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Match(models.Model):
     team_name1 = models.ForeignKey(Teams, on_delete=models.PROTECT, related_name='team1')
@@ -98,6 +74,3 @@ class Match(models.Model):
     field_name = models.ForeignKey(Field, on_delete=models.PROTECT)
     referee_name = models.ForeignKey(Referees, on_delete=models.PROTECT)
     date = models.DateField()
-
-    def __str__(self):
-        return "{} vs {}".format(self.team_name1.__str__(), self.team_name2.__str__()) 
